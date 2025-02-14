8db3d80c29cbd7aeb05c877c19cd4b6ebb18071c
Aadhavan Sridharan <151992049+aksamban@users.noreply.github.com>
2025-02-14 09:30:06 +0530

    nvm this is the first working ver (pls)

--- popularl/__init__.py
+++ popularl/__init__.py
@@ -1,7 +1,7 @@
-from core.train import train
-from core.train_maml import train_maml
-from core.tester import test
-from core.tester_maml import test_maml
-from core.utils import set_seed
+from .core.train import train
+from .core.train_maml import train_maml
+from .core.tester import test
+from .core.tester_maml import test_maml
+from .core.utils import set_seed
 
 set_seed(42)
